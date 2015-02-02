83f80f7f63ab53f4d945930d82fb386a19a5ba1c
Joe Sak <joe@joesak.com>
2015-02-01 23:43:13 -0500

    Add root route to label_maker/case_filters#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  namespace :label_maker do
-  get 'case_filters/index'
-  end
-
   namespace :label_maker, path: '' do
     resources :case_filters, only: [:index, :show]
   end
+  root to: 'label_maker/case_filters#index'
 end
