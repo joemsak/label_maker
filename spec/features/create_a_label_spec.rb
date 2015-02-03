0369499d2576011e46f96eb66e6e184eb2fcdf36
Joe Sak <joe@joesak.com>
2015-02-02 19:54:45 -0500

    Fix label creation test

--- spec/features/create_a_label_spec.rb
+++ spec/features/create_a_label_spec.rb
@@ -2,17 +2,17 @@ require 'rails_helper'
 
 RSpec.feature 'Create a label' do
   scenario 'Add a label to the list' do
-    VCR.use_cassette('create a label', record: :all) do
+    VCR.use_cassette('create a label') do
       visit label_maker_labels_path
 
       click_link I18n.t('links.controllers.labels.new')
 
-      fill_in :label_maker_label_name, with: 'My new label'
+      fill_in :label_maker_label_name, with: 'Tool Time'
       click_button I18n.t('forms.buttons.submit')
 
-      expect(page).to have_css('.flash.notice',
+      expect(page).to have_css('.alert-box.notice',
                            text: I18n.t('text.controllers.labels.create.success'))
-      expect(page).to have_css('.label.default', text: 'My new label')
+      expect(page).to have_css('.label.default', text: 'Tool Time')
     end
   end
 end
